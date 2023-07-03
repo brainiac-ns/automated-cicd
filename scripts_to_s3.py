837ecfe61e7742f04d2de3ff1b8e9c000e064627
stefan <stefan@lifebit.ai>
2023-07-03 18:23:24 +0200

    bug fix

--- scripts_to_s3.py
+++ scripts_to_s3.py
@@ -1,33 +1,39 @@
-import os
 import subprocess
 
 import boto3
 
 s3_bucket = "mlops-task"
 s3_folder = "scripts/"
-s3_client = boto3.client(
-    "s3",
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-)
+s3_client = boto3.client("s3")
 
-diff_output = (
-    subprocess.check_output(["git", "diff", "--name-status", "HEAD~1..HEAD"])
+# Run 'git log' command to get the list of changed files in the latest commit
+log_output = (
+    subprocess.check_output(["git", "log", "--name-status", "-1", "HEAD"])
     .decode()
     .strip()
 )
 
-for line in diff_output.split("\n"):
-    status, file_path = line.split("\t")
+files = []
+start_index = log_output.index("\n") + 1
 
-    if "scripts/" not in file_path:
+for line in log_output[start_index:].split("\n"):
+    if line.startswith(" "):
+        # Skip lines that don't represent changed files
         continue
-    print(f"File path: {file_path}")
-    if status == "A" or status == "M":
-        s3_key = s3_folder + file_path
-        s3_client.upload_file(file_path, s3_bucket, s3_key)
-        print(f"Uploaded file to S3: {s3_key}")
-    elif status == "D":
-        s3_key = s3_folder + file_path
-        s3_client.delete_object(Bucket=s3_bucket, Key=s3_key)
-        print(f"Deleted file from S3: {s3_key}")
+    status, file_path = line.split("\t")
+    files.append(file_path)
+
+for file_path in files:
+    if file_path.startswith("scripts/"):
+        status = file_path[0]
+
+        if status == "A" or status == "M":
+            # Upload file to S3
+            s3_key = s3_folder + file_path
+            s3_client.upload_file(file_path, s3_bucket, s3_key)
+            print(f"Uploaded file to S3: {s3_key}")
+        elif status == "D":
+            # Delete file from S3
+            s3_key = s3_folder + file_path
+            s3_client.delete_object(Bucket=s3_bucket, Key=s3_key)
+            print(f"Deleted file from S3: {s3_key}")
