7b1a81b2d5ca468ae8d6f26759f835e7588bf29b
stefan <stefan@lifebit.ai>
2023-07-03 18:35:15 +0200

    Delete test 2

--- scripts_to_s3.py
+++ scripts_to_s3.py
@@ -24,11 +24,12 @@ for line in log_output[start_index:].split("\n"):
 for file in files:
     status, file_path = file.split("\t")
     if file_path.startswith("scripts/"):
+        print(status, file_path)
         if status == "A" or status == "M":
             s3_key = file_path
             s3_client.upload_file(file_path, s3_bucket, s3_key)
-            print(f"Uploaded file to S3: {s3_key}")
+            # print(f"Uploaded file to S3: {s3_key}")
         elif status == "D":
             s3_key = file_path
             s3_client.delete_object(Bucket=s3_bucket, Key=s3_key)
-            print(f"Deleted file from S3: {s3_key}")
+            # print(f"Deleted file from S3: {s3_key}")
